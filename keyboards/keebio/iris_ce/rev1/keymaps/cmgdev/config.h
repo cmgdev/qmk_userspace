05b906f5aafd8a851ea5fa8778346166339a41b6
cmgdev <854806+cmgdev@users.noreply.github.com>
2025-10-09 13:32:32 -0400

    Update config.h

--- keyboards/keebio/iris_ce/rev1/keymaps/cmgdev/config.h
+++ keyboards/keebio/iris_ce/rev1/keymaps/cmgdev/config.h
@@ -1,9 +1,12 @@
 #pragma once
 
-#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+// This enables syncing of the layer state between both halves of the split keyboard. 
+// The main purpose of this feature is to enable support for use of things like OLED display of the currently active layer.
+// source https://docs.qmk.fm/features/split_keyboard#data-sync-options
+#define SPLIT_LAYER_STATE_ENABLE 
 
-// RGB Matrix Defaults
-#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR // Sets the default rgb mode to solid color
-#define RGB_MATRIX_DEFAULT_HUE 0 // Sets the default hue value, if none has been set
-#define RGB_MATRIX_DEFAULT_SAT 255 // Sets the default saturation value, if none has been set
-#define RGB_MATRIX_DEFAULT_VAL 100 // Sets the default brightness value, if none has been set
+// activate Caps Word by double-tapping left shift
+// This method works with KC_LSFT or one-shot Left Shift OSM(MOD_LSFT). 
+// To count as a double tap, the maximum time in milliseconds between taps is TAPPING_TERM, or if using TAPPING_TERM_PER_KEY, the time returned by get_tapping_term() for the shift keycode being tapped
+// source https://docs.qmk.fm/features/caps_word#how-do-i-enable-caps-word
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
