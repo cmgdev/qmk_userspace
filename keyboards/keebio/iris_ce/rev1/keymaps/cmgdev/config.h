2f457fcee7c3488d8ec5a2e563af5ec94c6ec2a3
cmgdev <854806+cmgdev@users.noreply.github.com>
2025-10-09 11:48:31 -0400

    Update config.h

--- keyboards/keebio/iris_ce/rev1/keymaps/cmgdev/config.h
+++ keyboards/keebio/iris_ce/rev1/keymaps/cmgdev/config.h
@@ -4,6 +4,6 @@
 
 // RGB Matrix Defaults
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR // Sets the default rgb mode to solid color
-#define RGB_MATRIX_DEFAULT_HUE HSV_CORAL // Sets the default hue value, if none has been set
+#define RGB_MATRIX_DEFAULT_HUE 0 // Sets the default hue value, if none has been set
 #define RGB_MATRIX_DEFAULT_SAT 255 // Sets the default saturation value, if none has been set
 #define RGB_MATRIX_DEFAULT_VAL 100 // Sets the default brightness value, if none has been set
